3ec5e688cb7f5a4498b7bd48fe5c815c305d42c5
reinchemo <chemorein24@gmail.com>
2023-02-16 10:16:22 -0800

    initial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- *main - A program that print a line using the printf function
- *Return: 0(Success)
+ * main - Entry point
+ * Return: Always return 0 (Success)
  */
 
 int main(void)
-	{
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);	
-	}
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
