fe6670afba10e6d02d7d127b2f635a0e0a2ae5c5
reinchemo <chemorein24@gmail.com>
2023-02-16 10:24:13 -0800

    initial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - Entry point
+*Return: 1
+*/
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
