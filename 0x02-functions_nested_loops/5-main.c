6719a3dff9eaa429f446183bfea3537cf2df8cba
reinchemo <chemorein24@gmail.com>
2023-02-21 01:06:21 -0800

    6

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -22,7 +22,7 @@ int main(void)
 	    r = print_sign(0xff);
 	    _putchar(',');
 	    _putchar(' ');
-	    _putchar(r + '0')
+	    _putchar(r + '0');
 	    _putchar('\n');						  r = print_sign(-1);
             _putchar(',');						  _putchar(' ');
 	    _putchar(r + '0');
